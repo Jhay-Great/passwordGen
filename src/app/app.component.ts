2ca445f5af2f6ad9d9d0e444aaea276c1df520a0
Jhay-Great <alemonai.jenef@gmail.com>
2024-07-15 23:51:01 +0000

    feat: password been generated and displayed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
@@ -14,6 +15,14 @@ export class AppComponent implements OnInit {
   copied:boolean = false;
   charLength:number = 0;
   placeholderText:string = 'sf@siA92*aF';
+  generatedPassword!:string;
+
+  formState = {
+    lowercase: false,
+    uppercase: false,
+    numbers: false,
+    symbols: false,
+  }
 
   // password letters, symbols and numbers
   lowercaseLetters:string = 'abcdefghijklmonpqrstuvwxyz';
@@ -22,9 +31,29 @@ export class AppComponent implements OnInit {
   symbols:string = '!@#$%^&*()_+[]{}|;:,.<>?'; 
 
 
+
   ngOnInit(): void {
     // console.log(this.evaluateCriteria(true, true, true, true));
     // console.log(this.generateRandomPassword(8, this.evaluateCriteria(true, true, true, true)))
+  }
+
+  onCheckboxChange() {
+    // console.log('called');
+    // console.log('Uppercase state changed:', this.formState);
+  }
+
+  onGenerate () {
+    const {uppercase, lowercase, numbers, symbols} = this.formState;
+    
+    const criteria = this.evaluateCriteria(lowercase, uppercase, numbers, symbols);
+
+    const password = this.generateRandomPassword(criteria);
+
+    this.generatedPassword = password;
+
+    return;
+
+
   }
 
   evaluateCriteria (
@@ -49,13 +78,13 @@ export class AppComponent implements OnInit {
     return Math.floor(Math.random() * length);
   }
 
-  generateRandomPassword (length:number, criteria:string) {
-    console.log(length)
+  generateRandomPassword ( criteria:string, length:number = 8) {
     let password = '';
     for (let i = 0; i < length; i++) {
       password += criteria[this.random(criteria.length)];
     }
 
+    // this.generatedPassword = password;
     return password;
   }
   
