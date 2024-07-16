8e0b47a47b6903871cfba847e1a85cd28988dd31
Jhay-Great <alemonai.jenef@gmail.com>
2024-07-16 07:42:21 +0000

    feature: strength functionality implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
+// import { Clipboard } from '@angular/cdk/clipboard';
+// import { ClipboardModule } from '@angular/cdk/clipboard';
 
 @Component({
   selector: 'app-root',
@@ -16,6 +18,7 @@ export class AppComponent implements OnInit {
   charLength:number = 0;
   placeholderText:string = 'sf@siA92*aF';
   generatedPassword!:string;
+  passwordStrength!:string;
 
   formState = {
     lowercase: false,
@@ -46,10 +49,14 @@ export class AppComponent implements OnInit {
     const {uppercase, lowercase, numbers, symbols} = this.formState;
     
     const criteria = this.evaluateCriteria(lowercase, uppercase, numbers, symbols);
-
+    
+    
     const password = this.generateRandomPassword(criteria);
-
+    
     this.generatedPassword = password;
+    const strength = this.evaluateStrength(lowercase, uppercase, numbers, symbols);
+    this.passwordStrength = strength as string;
+    console.log(this.passwordStrength);
 
     return;
 
@@ -86,6 +93,30 @@ export class AppComponent implements OnInit {
 
     // this.generatedPassword = password;
     return password;
+  }
+
+  // evaluating strength
+  evaluateStrength (
+    lowercase:boolean, 
+    uppercase:boolean, 
+    numbers:boolean, 
+    symbols:boolean
+  ) {
+    // const { uppercase, lowercase, numbers, symbols } = this.formState
+    let characterCount = 0;
+
+    if (this.generatedPassword.length < 8) return `Too weak`;
+    if (uppercase) characterCount++;
+    if (lowercase) characterCount++;
+    if (numbers) characterCount++;
+    if (symbols) characterCount++;
+
+    if (this.generatedPassword.length >= 8 && characterCount === 1) return 'weak';
+    if (this.generatedPassword.length >= 8 && characterCount >= 2) return 'medium';
+    if (this.generatedPassword.length >= 12 && characterCount >= 3) return 'strong';
+
+    return;
+
   }
   
 }
